529c6f87f6c64e18caec540b5ac04598f982e46d
J4Admin <159776479+J4Admin@users.noreply.github.com>
2024-08-15 11:33:48 +0200

    initial commit : with express/ server.js

---
+++ front-end/src/components/BackArrow/BackArrow.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowLeft } from '@fortawesome/free-solid-svg-icons';
+
+function BackArrow() {
+  return (
+    <Link to="/" className="backArrow">
+      {' '}
+      <FontAwesomeIcon icon={faArrowLeft} />
+      {' Retour'}
+    </Link>
+  );
+}
+
+export default BackArrow;
