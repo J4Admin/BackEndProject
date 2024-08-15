63fc4234e016b12dd4cb7ec8474261f78a0750cf
J4Admin <159776479+J4Admin@users.noreply.github.com>
2024-08-15 18:23:52 +0200

    fix: mongoose.connect, with async function. adding : CRUD logic in app.js

--- backend/app.js
+++ backend/app.js
@@ -1,17 +1,19 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
-const Books = require("./models/books");
-const app = express();
+const Book = require("./models/book");
 
 async function connectMongo() {
-  await mongoose.connect(
+  await mongoose
+    .connect(
       "mongodb+srv://Admin:Admin123@cluster0.mmwuj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
     )
     .then(() => console.log("Connexion à MongoDB réussie !"))
     .catch((e) => console.log(e));
 }
 connectMongo();
+
+const app = express();
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
@@ -30,27 +32,38 @@ app.use(bodyParser.json());
 
 app.post("/api/books", (req, res, next) => {
   delete req.body._id;
-  const books = new Books({
+  const book = new Book({
     ...req.body,
   });
-  books
+
+  book
     .save()
     .then(() => res.status(201).json({ message: "Le livre a été ajouté !" }))
     .catch((error) => res.status(400).json({ error }));
 });
 
-app.get("/api/user", (req, res, next) => {
-  const User = [
-    {
-      email: "bob",
-      password: "bricoleur",
-    },
-    {
-      email: "mickey",
-      password: "moose",
-    },
-  ];
-  res.status(200).json(User);
+app.put("/api/books/:id", (req, res, next) => {
+  Book.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
+    .then(() => res.status(200).json("Le livre à était modifié !"))
+    .catch((error) => res.status(400).json({ error }));
+});
+
+app.delete("/api/books/:id", (req, res, next) => {
+  Book.deleteOne({ _id: req.params.id })
+    .then(() => res.status(200).json("Objet suprimé !"))
+    .catch((error) => res.status(400).json({ error }));
+});
+
+app.get("/api/books/:id", (req, res, next) => {
+  Book.findOne({ _id: req.params.id })
+    .then((book) => res.status(200).json(book))
+    .catch((error) => res.status(400).json({ error }));
+});
+
+app.get("/api/books", (req, res, next) => {
+  Book.find()
+    .then((books) => res.status(200).json(books))
+    .catch((error) => res.status(400).json({ error }));
 });
 
 module.exports = app;
--- backend/models/books.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const booksSchema = mongoose.Schema({
-  title: { type: String, require: true },
-  image: { type: String, require: true },
-});
-
-module.exports = mongoose.model("books", booksSchema);
