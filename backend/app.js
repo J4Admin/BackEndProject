f21ea39f383dca04125b4377dff41a9a263a0fbf
J4Admin <159776479+J4Admin@users.noreply.github.com>
2024-08-15 18:49:23 +0200

    add: controller for export work logic, import into router.

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
-const Book = require("./models/book");
+
+const booksRoutes = require("./routes/books.js");
 
 async function connectMongo() {
   await mongoose
@@ -30,40 +31,6 @@ app.use((req, res, next) => {
 
 app.use(bodyParser.json());
 
-app.post("/api/books", (req, res, next) => {
-  delete req.body._id;
-  const book = new Book({
-    ...req.body,
-  });
-
-  book
-    .save()
-    .then(() => res.status(201).json({ message: "Le livre a été ajouté !" }))
-    .catch((error) => res.status(400).json({ error }));
-});
-
-app.put("/api/books/:id", (req, res, next) => {
-  Book.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
-    .then(() => res.status(200).json("Le livre à était modifié !"))
-    .catch((error) => res.status(400).json({ error }));
-});
-
-app.delete("/api/books/:id", (req, res, next) => {
-  Book.deleteOne({ _id: req.params.id })
-    .then(() => res.status(200).json("Objet suprimé !"))
-    .catch((error) => res.status(400).json({ error }));
-});
-
-app.get("/api/books/:id", (req, res, next) => {
-  Book.findOne({ _id: req.params.id })
-    .then((book) => res.status(200).json(book))
-    .catch((error) => res.status(400).json({ error }));
-});
-
-app.get("/api/books", (req, res, next) => {
-  Book.find()
-    .then((books) => res.status(200).json(books))
-    .catch((error) => res.status(400).json({ error }));
-});
+app.use("/api/books", booksRoutes);
 
 module.exports = app;
