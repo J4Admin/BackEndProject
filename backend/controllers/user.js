dfd725a54c58f88e0510f9fa84553372a2b58eb3
J4Admin <159776479+J4Admin@users.noreply.github.com>
2024-08-16 11:59:22 +0200

    add: dependencies bcrypt, jwt and  fuction login  in user.js controller

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,4 +1,6 @@
 const User = require("../models/User");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
 
 exports.signup = (req, res, next) => {
   bcrypt
@@ -11,8 +13,41 @@ exports.signup = (req, res, next) => {
       user
         .save()
         .then(() => res.status(201).json({ message: "Utilisateur créé" }))
-        .catch((error) => req.status(400).json({ error }));
+        .catch((error) => res.status(400).json({ error }));
     })
-    .catch((error) => req.status(500).json({ error }));
+    .catch((error) => res.status(500).json({ error }));
 };
-exports.login = (req, res, next) => {};
+
+exports.login = (req, res, next) => {
+  User.findOne({ email: req.body.email })
+    .then((user) => {
+      if (user === null) {
+        res
+          .status(401)
+          .json({ message: "Paire identifiant/mot de passe incorrecte" });
+      } else {
+        bcrypt
+          .compare(req.body.password, user.password)
+          .then((valid) => {
+            if (!valid) {
+              res
+                .status(401)
+                .json({ message: "Paire identifiant/mot de passe incorrecte" });
+            } else {
+              res.status(200).json({
+                userId: user._id,
+                token: jwt.sign({ userId: user._id }, "RANDOM_TOKEN_SECRET", {
+                  expiresIn: "24h",
+                }),
+              });
+            }
+          })
+          .catch((error) => {
+            res.status(500).json({ error });
+          });
+      }
+    })
+    .catch((error) => {
+      res.status(500).json({ error });
+    });
+};
