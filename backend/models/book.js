ca25a5eed47a341b61a1f2cc94d3e8f06c0e7208
J4Admin <159776479+J4Admin@users.noreply.github.com>
2024-08-17 00:54:22 +0200

    fix: midlewares in routes for CRUD bugs

--- backend/models/book.js
+++ backend/models/book.js
@@ -1,19 +1,29 @@
 const mongoose = require("mongoose");
+const { Schema } = mongoose;
 
-const bookSchema = mongoose.Schema({
-  userId: { type: String, required: true },
-  title: { type: String, required: true },
-  author: { type: String, required: true },
-  imageUrl: { type: String, required: true },
-  year: { type: String, required: true },
-  genre: { type: String, required: true },
-  ratings: [
-    {
-      userId: { type: String, required: true },
-      grade: { type: Number, required: true },
-    },
-  ],
-  averageRating: { type: Number, required: true },
+const userReference = {
+  type: Schema.Types.ObjectId,
+  ref: "User",
+  required: true,
+};
+
+const ratingSchema = new Schema({
+  userId: userReference,
+  grade: { type: Number, min: 0, max: 5, required: true },
 });
 
-module.exports = mongoose.model("book", bookSchema);
+const bookSchema = new Schema([
+  {
+    userId: { type: String, required: true },
+    title: { type: String, required: true },
+    author: { type: String, required: true },
+    imageUrl: { type: String, required: true },
+    year: { type: Number, required: true },
+    genre: { type: String, required: true },
+    ratings: [ratingSchema],
+    averageRating: { type: Number, default: 0 },
+  },
+]);
+
+const Book = mongoose.model("Book", bookSchema);
+module.exports = Book;
